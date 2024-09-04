8d77e91494cf2bf9245ecf8e5308c47ae443be1e
lukasbicus <ing.lukas.bicus@gmail.com>
2024-09-04 08:03:50 +0200

    Add dummy functions with tests

---
+++ scripts/aoc2015/day8/utils.test.ts
@@ -0,0 +1,45 @@
+import { assertEquals, assertThrows } from "jsr:@std/assert@1";
+import { beforeEach, describe, it } from "@std/testing/bdd";
+import { getCodeLength } from "./utils.ts";
+
+// string     code length                     string length
+// ""         2                               0
+// "abc"      5                               3
+// "aaa\"aaa" 10                              7
+// "aaa\\aaa" 10                              7
+// "\x27"     6                               1
+
+describe("getCodeLength", function () {
+  it('Should return length of ""', function () {
+    assertEquals(getCodeLength('""'), 2);
+  });
+  it('Should return length of "abc"', function () {
+    assertEquals(getCodeLength('"abc"'), 5);
+  });
+  it('Should return length of "aaa"aaa"', function () {
+    assertEquals(getCodeLength('"aaa"aaa"'), 10);
+  });
+  it('Should return length of "aaa\\aaa"', function () {
+    assertEquals(getCodeLength('"aaa\\aaa"'), 10);
+  });
+  it('Should return length of "\x27"', function () {
+    assertEquals(getCodeLength('"\x27"'), 6);
+  });
+});
+describe("getStringLength", function () {
+  it('Should return length of ""', function () {
+    assertEquals(getCodeLength('""'), 0);
+  });
+  it('Should return length of "abc"', function () {
+    assertEquals(getCodeLength('"abc"'), 3);
+  });
+  it('Should return length of "aaa"aaa"', function () {
+    assertEquals(getCodeLength('"aaa"aaa"'), 7);
+  });
+  it('Should return length of "aaa\\aaa"', function () {
+    assertEquals(getCodeLength('"aaa\\aaa"'), 7);
+  });
+  it('Should return length of "\x27"', function () {
+    assertEquals(getCodeLength('"\x27"'), 1);
+  });
+});
---
+++ scripts/aoc2015/day8/utils.ts
@@ -0,0 +1,10 @@
+// get code length - util
+
+export function getCodeLength(line: string) {
+  return line.length;
+}
+// get string length - util
+
+export function getStringLength(line: string) {
+  return line.length;
+}
