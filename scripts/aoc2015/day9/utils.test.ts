43e371aac3edbdd95968b4c1739ab2f177d0ddfd
lukasbicus <ing.lukas.bicus@gmail.com>
2024-09-16 15:49:15 +0200

    Add more failing tests.

--- scripts/aoc2015/day9/utils.test.ts
+++ scripts/aoc2015/day9/utils.test.ts
@@ -25,9 +25,28 @@ describe("utils", () => {
     });
   });
 
-  describe('for set {"A", "B"}', function () {
-    it('Should return result with arrays ["A", "B"], ["B", "A"] for empty currentArrays', function () {});
-    it('Should return result with arrays ["X", "A", "B"], ["X", "B", "A"] for currentArrays ["X"]', function () {});
+  describe.only('for set {"A", "B"}', function () {
+    const uncombinedValues = new Set(["A", "B"]);
+    it('Should return result with arrays ["A", "B"], ["B", "A"] for empty currentArrays', function () {
+      assertEquals(combine([[]], uncombinedValues), [
+        ["A", "B"],
+        ["B", "A"],
+      ]);
+    });
+    it('Should return result with arrays ["X", "A", "B"], ["X", "B", "A"] for currentArrays [["X"]]', function () {
+      assertEquals(combine([["X"]], uncombinedValues), [
+        ["X", "A", "B"],
+        ["X", "B", "A"],
+      ]);
+    });
+    it('Should add arrays ["A", "B"], ["B", "A"] to every array in currentArrays', function () {
+      assertEquals(combine([["X", "Y"], ["Y", "X"]], uncombinedValues), [
+        ["X", "Y", "A", "B"],
+        ["X", "Y", "B", "A"],
+        ["Y", "X", "A", "B"],
+        ["Y", "X", "B", "A"],
+      ]);
+    });
   });
 
   describe('for set {"A", "B", "C"}', function () {
--- scripts/aoc2015/day9/utils.ts
+++ scripts/aoc2015/day9/utils.ts
@@ -1,3 +1,35 @@
+function separateSetValue<T>(
+  set: Set<T>,
+): { value: T; setWithoutValue: Set<T> } {
+  if (set.size === 0) {
+    throw new Error("Set is empty");
+  }
+  let valueIsSet = false;
+  let value: T;
+  const newSet = new Set<T>();
+  for (const item of set.values()) {
+    if (!valueIsSet) {
+      value = item;
+      valueIsSet = true;
+    } else {
+      newSet.add(item);
+    }
+  }
+  return {
+    value: value!,
+    setWithoutValue: newSet,
+  };
+}
+
+// export function combineExistingArrayWithSetValues<T>(arr: T[], set: Set<T>) {
+//   let arrraysToAppend: T[];
+//   for (const item of set.values()) {
+//     const setWithoutItem = new Set<T>(set);
+//     setWithoutItem.delete(item);
+//     arrraysToAppend.push(combine(arr, ));
+//   }
+// }
+
 export function combine<T>(
   currentArrays: T[][],
   uncombinedValues: Set<T>,
@@ -10,6 +42,13 @@ export function combine<T>(
     return currentArrays.map((currentArray) =>
       currentArray.concat(Array.from(uncombinedValues))
     );
+  }
+  if (uncombinedValues.size > 1) {
+    const { value, setWithoutValue } = separateSetValue(uncombinedValues);
+    return currentArrays.reduce((acc: T[][], currentArray) => {
+      // concat current array with one value from set
+      return acc.concat;
+    }, [[]]);
   }
   return [];
 }
