e637fcd8e2e902c0bda89fc01b0a2a3b60aa5bf5
lukasbicus <ing.lukas.bicus@gmail.com>
2024-09-23 11:35:44 +0200

    Implement getNumbersFromText

--- scripts/aoc2015/day12/utils.test.ts
+++ scripts/aoc2015/day12/utils.test.ts
@@ -7,7 +7,7 @@ describe("getNumbersFromText", () => {
     assertEquals(getNumbersFromText("abc"), []);
   });
   it("should return array of numbers for text with numbers", function () {
-    assertEquals(getNumbersFromText("[1,2,3]"), [1, 2, 3]);
+    assertEquals(getNumbersFromText("[1,-2,3]"), [1, -2, 3]);
     assertEquals(getNumbersFromText(`{"a":2,"b":4}`), [2, 4]);
     assertEquals(getNumbersFromText("[[[3]]]"), [3]);
     assertEquals(getNumbersFromText(`{"a":{"b":4},"c":-1}`), [4, -1]);
--- scripts/aoc2015/day12/utils.ts
+++ scripts/aoc2015/day12/utils.ts
@@ -1,3 +1,13 @@
+const numbersRegex = /(-)?([0-9]+)/g;
+
 export function getNumbersFromText(text: string): number[] {
-  return [];
+  const result: number[] = [];
+  const allMatchesIterator = text.matchAll(numbersRegex);
+  for (const match of allMatchesIterator) {
+    const num = parseInt(match[0], 10);
+    if (!Number.isNaN(num)) {
+      result.push(num);
+    }
+  }
+  return result;
 }
