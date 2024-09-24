5e1199186d7c37393ff9c047c372e7737d1530a2
lukasbicus <ing.lukas.bicus@gmail.com>
2024-09-24 12:17:32 +0200

    Add types

---
+++ scripts/aoc2015/day13/types.ts
@@ -0,0 +1,10 @@
+export enum Neighbor {
+  Alice = "Alice",
+  Bob = "Bob",
+  Carol = "Carol",
+  David = "David",
+  Eric = "Eric",
+  Frank = "Frank",
+  George = "George",
+  Mallory = "Mallory",
+}
