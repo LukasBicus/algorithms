043110787b9bac93a070500abe160d1eba612e9b
lukasbicus <ing.lukas.bicus@gmail.com>
2024-09-25 12:23:37 +0200

    Clean comments in tests.

--- scripts/aoc2015/day14/utils.test.ts
+++ scripts/aoc2015/day14/utils.test.ts
@@ -32,14 +32,12 @@ describe("Reindeer", function () {
     });
   });
 
-  // After one second Dancer has gone 16 km.
   it("After one second Dancer has gone 16 km.", function () {
     reindeer.tick();
     assertEquals(reindeer.currentActivity, ReindeerActivity.Flying);
     assertEquals(reindeer.distanceTraveled, 16);
     assertEquals(reindeer.timeSpentOnActivity, 1);
   });
-  // After ten seconds Dancer has gone 160 km.
   it("After 10 seconds Dancer has gone 160 km.", function () {
     for (let i = 0; i < 10; i++) {
       reindeer.tick();
@@ -48,7 +46,6 @@ describe("Reindeer", function () {
     assertEquals(reindeer.distanceTraveled, 160);
     assertEquals(reindeer.timeSpentOnActivity, 10);
   });
-  // On the eleventh second Dancer continues on for a total distance of 176 km.
 
   it("After 11 seconds Dancer has gone 176 km.", function () {
     for (let i = 0; i < 11; i++) {
@@ -58,7 +55,6 @@ describe("Reindeer", function () {
     assertEquals(reindeer.distanceTraveled, 176);
     assertEquals(reindeer.timeSpentOnActivity, 0);
   });
-  // On the 12th second Dancer stays at 176
   it("After 12 seconds Dancer has gone 176 km.", function () {
     for (let i = 0; i < 12; i++) {
       reindeer.tick();
@@ -75,5 +71,4 @@ describe("Reindeer", function () {
     assertEquals(reindeer.distanceTraveled, 192);
     assertEquals(reindeer.timeSpentOnActivity, 1);
   });
-  // On the 174th second, Dancer distance is 176 + 16.
 });
