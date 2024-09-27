0a7256425b182dc8da1fb178a2faecba483c2d59
lukasbicus <ing.lukas.bicus@gmail.com>
2024-09-27 14:25:59 +0200

    Implement computeScore

--- scripts/aoc2015/day15/utils.test.ts
+++ scripts/aoc2015/day15/utils.test.ts
@@ -1,6 +1,6 @@
 import { describe, it } from "@std/testing/bdd";
-import { assertEquals } from "@std/assert";
-import { parseIngredientLine } from "./utils.ts";
+import { assertEquals, assertThrows } from "@std/assert";
+import { computeScore, parseIngredientLine } from "./utils.ts";
 
 describe("parseIngredientLine", function () {
   it("should return null for invalid line", function () {
@@ -35,6 +35,51 @@ describe("parseIngredientLine", function () {
         texture: 3,
         calories: 8,
       },
+    );
+  });
+});
+
+describe("computeScore", function () {
+  const ingredients = {
+    Butterscotch: {
+      name: "Butterscotch",
+      capacity: -1,
+      durability: -2,
+      flavor: 6,
+      texture: 3,
+      calories: 8,
+    },
+    Cinnamon: {
+      name: "Cinnamon",
+      capacity: 2,
+      durability: 3,
+      flavor: -2,
+      texture: -1,
+      calories: 3,
+    },
+  };
+  it("should throw error, when ingredient is not found", function () {
+    assertThrows(() =>
+      computeScore({
+        Butterscotch: 1,
+      }, {})
+    );
+  });
+  it("should return 0, when an property for combination of ingredients is negative", function () {
+    assertEquals(
+      computeScore({
+        Butterscotch: 1,
+      }, ingredients),
+      0,
+    );
+  });
+  it("should return a value for combination of ingredients", function () {
+    assertEquals(
+      computeScore({
+        Butterscotch: 44,
+        Cinnamon: 56,
+      }, ingredients),
+      62842880,
     );
   });
 });
--- scripts/aoc2015/day15/utils.ts
+++ scripts/aoc2015/day15/utils.ts
@@ -24,3 +24,50 @@ export function parseIngredientLine(line: string): Ingredient | null {
   }
   return null;
 }
+
+type RecipeProperties = {
+  capacity: number;
+  durability: number;
+  flavor: number;
+  texture: number;
+};
+
+export function computeScore(
+  combination: Record<string, number>,
+  ingredients: Record<string, Ingredient>,
+): number {
+  const ingredientNames = Object.keys(combination);
+  ingredientNames.forEach((name) => {
+    if (!ingredients[name]) {
+      throw new Error("Missing ingredient " + name);
+    }
+  });
+  const recipeProperties: RecipeProperties = {
+    capacity: 0,
+    durability: 0,
+    flavor: 0,
+    texture: 0,
+  };
+  for (
+    const property of [
+      "capacity",
+      "durability",
+      "flavor",
+      "texture",
+    ]
+  ) {
+    let value = 0;
+    for (const [name, spoonsCount] of Object.entries(combination)) {
+      const ingredient = ingredients[name];
+      value = value + spoonsCount * (ingredient[
+            property as keyof RecipeProperties
+          ]);
+    }
+    if (value <= 0) {
+      return 0;
+    }
+    recipeProperties[property as keyof RecipeProperties] = value;
+  }
+  return recipeProperties.flavor * recipeProperties.texture *
+    recipeProperties.capacity * recipeProperties.durability;
+}
