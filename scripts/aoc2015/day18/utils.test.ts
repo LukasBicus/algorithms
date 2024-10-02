ddcec8be2145f405b40bbeb96d76d1d88b536e42
lukasbicus <ing.lukas.bicus@gmail.com>
2024-10-02 11:34:02 +0200

    Day18 (#15)
    * Add day 18.
    
    * Add algorithm.
    
    * Add dummy getNeighboursPositions with one test
    
    * Add more tests for getNeighboursPositions
    
    * Add tests for fillGrid
    
    * Implement fillGrid
    
    * Implement getNeighboursPositions
    
    * Implement performStep with test
    
    * Solve demo input.
    
    * Solve input.
    
    * Implement litGridCorners
    
    * Solve puzzle2

---
+++ scripts/aoc2015/day18/utils.test.ts
@@ -0,0 +1,99 @@
+import { assertEquals } from "@std/assert";
+import { describe, it } from "@std/testing/bdd";
+import {
+  fillGrid,
+  getNeighboursPositions,
+  LightGrid,
+  LightState,
+  litGridCorners,
+  performStep,
+} from "./utils.ts";
+
+describe("getNeighboursPositions", () => {
+  it("should return position for {x: 1, y:2}", function () {
+    assertEquals(getNeighboursPositions({ x: 1, y: 2 }, 6), [
+      { x: 0, y: 1 },
+      { x: 1, y: 1 },
+      { x: 2, y: 1 },
+      { x: 0, y: 2 },
+      { x: 2, y: 2 },
+      { x: 0, y: 3 },
+      { x: 1, y: 3 },
+      { x: 2, y: 3 },
+    ]);
+  });
+
+  it("should return position for {x: 0, y: 3}", function () {
+    assertEquals(getNeighboursPositions({ x: 0, y: 3 }, 6), [
+      { x: 0, y: 2 },
+      { x: 1, y: 2 },
+      { x: 1, y: 3 },
+      { x: 0, y: 4 },
+      { x: 1, y: 4 },
+    ]);
+  });
+
+  it("should return position for {x: 3, y: 0}", function () {
+    assertEquals(getNeighboursPositions({ x: 3, y: 0 }, 6), [
+      { x: 2, y: 0 },
+      { x: 4, y: 0 },
+      { x: 2, y: 1 },
+      { x: 3, y: 1 },
+      { x: 4, y: 1 },
+    ]);
+  });
+});
+
+describe("fillGrid", function () {
+  it("should fill simpleGrid", function () {
+    const expectedGrid: LightGrid = new Map();
+    expectedGrid.set("0-0", LightState.TurnedOff);
+    expectedGrid.set("0-1", LightState.TurnedOff);
+    expectedGrid.set("1-0", LightState.TurnedOn);
+    expectedGrid.set("1-1", LightState.TurnedOff);
+    assertEquals(
+      fillGrid(`.#
+..`),
+      {
+        grid: expectedGrid,
+        size: 2,
+      },
+    );
+  });
+});
+
+describe("performStep", function () {
+  it("should perform step", function () {
+    const { grid: initialGrid, size } = fillGrid(`.#.#.#
+...##.
+#....#
+..#...
+#.#..#
+####..`);
+    const { grid: expectedGrid } = fillGrid(`..##..
+..##.#
+...##.
+......
+#.....
+#.##..`);
+    assertEquals(performStep(initialGrid, size), expectedGrid);
+  });
+});
+
+describe("litGridCorners", function () {
+  it("should lit grid corners", function () {
+    const { grid: initialGrid, size } = fillGrid(`.#.#.#
+...##.
+#....#
+..#...
+#.#..#
+####..`);
+    const { grid: expectedGrid } = fillGrid(`##.#.#
+...##.
+#....#
+..#...
+#.#..#
+####.#`);
+    assertEquals(litGridCorners(initialGrid, size), expectedGrid);
+  });
+});
---
+++ scripts/aoc2015/day18/utils.ts
@@ -0,0 +1,141 @@
+export type Position = {
+  x: number;
+  y: number;
+};
+
+export enum LightState {
+  TurnedOff = "turnedOff",
+  TurnedOn = "turnedOn",
+}
+
+// starts with "0-0"
+// for grid with size 100 ends with "99-99"
+export type LightGrid = Map<string, LightState>;
+
+// get neighbours function (position - position of a light in a grid)
+// gets positions all neighbours of a light in a grid
+export function getNeighboursPositions(
+  pos: Position,
+  gridSize: number,
+): Position[] {
+  if (pos.x < 0 || pos.x >= gridSize || pos.y < 0 || pos.y >= gridSize) {
+    throw new Error("Position is not in grid");
+  }
+  const positions: Position[] = [];
+  for (const y of [pos.y - 1, pos.y, pos.y + 1]) {
+    if (y >= 0 && y < gridSize) {
+      for (const x of [pos.x - 1, pos.x, pos.x + 1]) {
+        if (x >= 0 && x < gridSize) {
+          if (!(x === pos.x && y === pos.y)) {
+            positions.push({ x, y });
+          }
+        }
+      }
+    }
+  }
+  return positions;
+}
+
+export function getGridKey(pos: Position) {
+  return `${pos.x}-${pos.y}`;
+}
+
+export function getPositionFromKey(key: string): Position {
+  const [x, y] = key.split("-");
+  return {
+    x: parseInt(x, 10),
+    y: parseInt(y, 10),
+  };
+}
+
+// fill a grid of 100x100
+// A # means "on", and a . means "off".
+// a "\n" means new line in grid
+export function fillGrid(input: string): { grid: LightGrid; size: number } {
+  const grid: LightGrid = new Map();
+  const lines = input.split("\n");
+  const size = lines.length;
+  for (const [y, line] of Object.entries(lines)) {
+    for (const [x, char] of Object.entries(line)) {
+      const key = getGridKey({
+        x: parseInt(x, 10),
+        y: parseInt(y, 10),
+      });
+      switch (char) {
+        case ".":
+          grid.set(
+            key,
+            LightState.TurnedOff,
+          );
+          break;
+        case "#":
+          grid.set(
+            key,
+            LightState.TurnedOn,
+          );
+          break;
+        default:
+          break;
+      }
+    }
+  }
+  return {
+    grid,
+    size,
+  };
+}
+
+export function performStep(prevGrid: LightGrid, size: number) {
+  // create next step with no data
+  const grid: LightGrid = new Map();
+  // for each light => get state of the light, get state of surrounding neighbours
+  for (const [key, state] of prevGrid) {
+    const surroundingPositions = getNeighboursPositions(
+      getPositionFromKey(key),
+      size,
+    );
+    const countOfLitNeighbours = surroundingPositions.reduce(
+      (acc, position) => {
+        const stateOfNeighbourLight = prevGrid.get(getGridKey(position));
+        if (stateOfNeighbourLight === LightState.TurnedOn) {
+          return acc + 1;
+        }
+        return acc;
+      },
+      0,
+    );
+    // A light which is on stays on when 2 or 3 neighbors are on, and turns off otherwise.
+    if (state === LightState.TurnedOn) {
+      grid.set(
+        key,
+        [2, 3].includes(countOfLitNeighbours)
+          ? LightState.TurnedOn
+          : LightState.TurnedOff,
+      );
+    } else {
+      // A light which is off turns on if exactly 3 neighbors are on, and stays off otherwise.
+      grid.set(
+        key,
+        (countOfLitNeighbours === 3)
+          ? LightState.TurnedOn
+          : LightState.TurnedOff,
+      );
+    }
+  }
+
+  // get neighbours function (position - position of a light in a grid)
+  // gets positions all neighbours of a light in a grid
+  return grid;
+}
+
+export function litGridCorners(grid: LightGrid, size: number) {
+  const cornerPosition = size - 1;
+  grid.set(getGridKey({ x: 0, y: 0 }), LightState.TurnedOn);
+  grid.set(getGridKey({ x: 0, y: cornerPosition }), LightState.TurnedOn);
+  grid.set(getGridKey({ x: cornerPosition, y: 0 }), LightState.TurnedOn);
+  grid.set(
+    getGridKey({ x: cornerPosition, y: cornerPosition }),
+    LightState.TurnedOn,
+  );
+  return grid;
+}
