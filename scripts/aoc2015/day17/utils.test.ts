228bca29b950fb80548ae07e08f198f6ca74604e
lukasbicus <ing.lukas.bicus@gmail.com>
2024-10-02 09:32:08 +0200

    Day17 (#14)
    * Add day17 puzzle1 with inputs
    
    * Implement parseContainerLine
    
    * Write simple tests.
    
    * Write more complex test for 3 items.
    
    * Implement generateCombinationsWithoutRepetition, fix tests.
    
    * Solve puzzle1.
    
    * Add algorithm for puzzle2
    
    * Solve puzzle2

---
+++ scripts/aoc2015/day17/utils.test.ts
@@ -0,0 +1,55 @@
+import { assertEquals } from "@std/assert";
+import { describe, it } from "@std/testing/bdd";
+import {
+  generateCombinationsWithoutRepetition,
+  parseContainerLine,
+} from "./utils.ts";
+
+describe("parseContainerLine", function () {
+  it("should return null for invalid line", function () {
+    assertEquals(parseContainerLine("invalid line"), null);
+  });
+
+  it("should return null for valid line", function () {
+    assertEquals(parseContainerLine("5"), 5);
+    assertEquals(parseContainerLine("25"), 25);
+  });
+});
+
+describe("generateCombinationsWithoutRepetition", function () {
+  it("should return an empty array, if length of input items is 0", function () {
+    const generator = generateCombinationsWithoutRepetition([]);
+    const result = generator.next();
+    assertEquals(result, {
+      value: [],
+      done: false,
+    });
+  });
+
+  it("should return 2 results, if length of input items is 1", function () {
+    const expectedResult = [[], ["a"]];
+    const result = [...generateCombinationsWithoutRepetition(["a"])];
+    assertEquals(result, expectedResult);
+  });
+
+  it("should return 4 results, if length of input items is 2", function () {
+    const expectedResult = [[], ["a"], ["b"], ["a", "b"]];
+    const result = [...generateCombinationsWithoutRepetition(["a", "b"])];
+    assertEquals(result, expectedResult);
+  });
+
+  it("should return 8 results, if length of input items is 3", function () {
+    const expectedResult = [
+      [],
+      ["a"],
+      ["b"],
+      ["a", "b"],
+      ["c"],
+      ["a", "c"],
+      ["b", "c"],
+      ["a", "b", "c"],
+    ];
+    const result = [...generateCombinationsWithoutRepetition(["a", "b", "c"])];
+    assertEquals(result, expectedResult);
+  });
+});
---
+++ scripts/aoc2015/day17/utils.ts
@@ -0,0 +1,57 @@
+const containerRegex = /(\d+)/;
+
+export function parseContainerLine(line: string): number | null {
+  const match = line.match(containerRegex);
+  return match ? parseInt(match[1]) : null;
+}
+
+// getCombinationsWithoutRepetition
+
+// for set of items {a, b}, available combinations are:
+// []
+// [a]
+// [b]
+// [a, b]
+
+// for set of items {a, b, c}, available combinations are:
+// []
+// [a]
+// [b]
+// [c]
+// [a, b]
+// [a, c]
+// [b, c]
+// [a, b, c]
+
+// those can be grouped into:
+
+// without item "a"
+// []
+// [b]
+// [c]
+// [b, c]
+
+// with item "a"- see - the item "a" is added in front of all options without "a"
+// [a]
+// [a, b]
+// [a, c]
+// [a, b, c]
+
+export function* generateCombinationsWithoutRepetition<T>(
+  items: T[],
+): Generator<T[]> {
+  if (items.length === 0) {
+    yield [];
+    return;
+  } else if (items.length === 1) {
+    yield [];
+    yield [items[0]];
+    // items.length > 1
+  } else {
+    const firstItem = items.shift() as T;
+    for (const combination of generateCombinationsWithoutRepetition(items)) {
+      yield combination;
+      yield [firstItem].concat(combination);
+    }
+  }
+}
