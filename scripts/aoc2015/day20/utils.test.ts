ab9500b3af763630cea04c0d7bdffea9e43bf270
lukasbicus <ing.lukas.bicus@gmail.com>
2024-10-04 11:45:58 +0200

    Work on decomposeToPrimeNumbers function.

--- scripts/aoc2015/day20/utils.test.ts
+++ scripts/aoc2015/day20/utils.test.ts
@@ -12,18 +12,25 @@ describe("decomposeToPrimeNumbers", function () {
     assertEquals(decomposeToPrimeNumbers(2, primeNumbers), [1, 2]);
   });
 
-  it.skip("should decompose 2 to prime numbers", function () {
+  it("should decompose 2 to prime numbers and add 2 to prime numbers", function () {
     const primeNumbers = [1];
     assertEquals(decomposeToPrimeNumbers(2, primeNumbers), [1, 2]);
     assertEquals(primeNumbers, [1, 2]);
   });
-  it.skip("should decompose 3 to prime numbers", function () {
-    assertEquals(decomposeToPrimeNumbers(3), [1, 3]);
-  });
-  it.skip("should decompose 3 to prime numbers", function () {
+  it("should decompose 3 to prime numbers and add 3 to prime numbers", function () {
     const primeNumbers = [1, 2];
     assertEquals(decomposeToPrimeNumbers(3, primeNumbers), [1, 3]);
     assertEquals(primeNumbers, [1, 2, 3]);
+  });
+  it.skip("should decompose 5 to prime numbers and add 5 to prime numbers", function () {
+    const primeNumbers = [1, 2, 3];
+    assertEquals(decomposeToPrimeNumbers(5, primeNumbers), [1, 5]);
+    assertEquals(primeNumbers, [1, 2, 3, 5]);
+  });
+  it.skip("should decompose 11 to prime numbers and add 11 to prime numbers", function () {
+    const primeNumbers = [1, 2, 3, 5, 7];
+    assertEquals(decomposeToPrimeNumbers(11, primeNumbers), [1, 11]);
+    assertEquals(primeNumbers, [1, 2, 3, 5, 7, 11]);
   });
 
   it.skip("should decompose 4 to prime numbers", function () {
--- scripts/aoc2015/day20/utils.ts
+++ scripts/aoc2015/day20/utils.ts
@@ -1,6 +1,6 @@
 export function decomposeToPrimeNumbers(
   num: number,
-  foundPrimeNumbers = [1],
+  knownPrimeNumbers = [1],
 ): number[] {
   if (num < 1) {
     throw new Error("Can decompose only numbers > 1");
@@ -8,8 +8,36 @@ export function decomposeToPrimeNumbers(
   if (num === 1) {
     return [1];
   }
-  if (foundPrimeNumbers.includes(num)) {
+  if (knownPrimeNumbers.includes(num)) {
     return [1, num];
   }
+
+  // case 1: num is 3
+  // limit is 3**0.5 cca 1.73...
+  const limit = num ** 0.5;
+  // list of prime numbers except 1 is [2]
+  const filteredKnownPrimeNumbers = knownPrimeNumbers.slice(1).filter((n) =>
+    n < limit
+  );
+  // list of prime numbers except 1 and bellow 1.7 is []
+  if (filteredKnownPrimeNumbers.length === 0) {
+    // there is no prime number to test
+    // add number to list of known prime numbers
+    knownPrimeNumbers.push(num);
+    return [1, num];
+  }
+  // check foundPrimeNumber of foundPrimeNumbers till foundPrimeNumber <= num**0.5
+  // if none of foundPrimeNumbers (except 1) divides the number with remainder 0
+
+  // case 1: num is 11
+  // limit is 11**0.5 cca 3.31...
+  // list of prime numbers except 1 is [2, 3, 5, 7]
+  // list of prime numbers except 1 and bellow 3.31 is [2, 3]
+  // test given prime numbers
+  // 11 % 2 is 1, not 0, continue
+  // 11 % 3 is 2, not 0, continue
+  // there are no more numbers -> add 11 to list of prime numbers
+  // return [1, 11] (1, new prime number)
+
   return [];
 }
